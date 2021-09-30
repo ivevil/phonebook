21763ddbd940748fa0af8b6b6e8c1fe291db246f
ivevil <ivevil@gmail.com>
2021-09-30 16:53:24 +0200

    Change the style of the app

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -45,7 +45,7 @@ class Footer extends React.Component {
         return (<footer>
             <input value={this.state.firstname} onChange={this.changeFirstName} id="firstname" type="text" placeholder="First Name" />
             <input value={this.state.lastname} onChange={this.changeLastName} id="lastname" type="text" placeholder="Last Name" />
-            <input value={this.state.number} onChange={this.changeNumber} id="number" type="text" placeholder="Number" />
+            <input maxlength="14" value={this.state.number} onChange={this.changeNumber} id="number" type="text" placeholder="Number" />
             <span id="add-btn" onClick={(e) => {
               this.props.createItem(this.state.firstname, this.state.lastname, this.state.number);
               this.resetFields();
